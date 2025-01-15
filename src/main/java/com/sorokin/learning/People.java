0ecc6358d21a50f25b5ac5b3cb9fb6d5001e365a
Sorokin1989 <sorokindm1try@yandex.ru>
2025-01-15 20:20:04 +0300

    init

---
+++ src/main/java/com/sorokin/learning/People.java
@@ -0,0 +1,19 @@
+package com.sorokin.learning;
+
+public class People {
+
+    private String firstName;
+
+    public People(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String getFirstName() {
+        return this.firstName;
+    }
+
+}
---
+++ src/main/java/com/sorokin/learning/TestService.java
@@ -0,0 +1,13 @@
+package com.sorokin.learning;
+
+public class TestService {
+
+    public TestService() {
+    }
+
+    public void test() {
+        People people = new People("Dima");
+        people.setFirstName("Maxim");
+    }
+
+}
